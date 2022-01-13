c188350f8c220f6857f1e5f149c55842c38ca426
p22strautz <p22strautz@fairfieldprep.org>
2022-01-13 10:06:02 -0500

    hw 9-1

--- hw9-1-3.py
+++ hw9-1-3.py
@@ -1,6 +1,15 @@
 # Author: SCT (AMDG) 1/12/22
 
-var = []
-
-def find_thing(string_list, element):
-    
+def find_thing(list_or_string, to_be_found):
+    for i, v in enumerate(list_or_string):
+        try:
+            if to_be_found in v:
+                print(i)
+                break
+            elif to_be_found not in list_or_string:
+                print(-1)
+                break
+                
+        except: print("Invalid Input")
+                
+find_thing("apple", "p")
